6af0c4b42a9ef630d6b5b386dd843a5c4cc6dfa4
ebrault <emmet.brault@gmail.com>
2019-04-04 16:38:39 -0400

    Added Boilerplate For Comment Component
    Added boilerplate JSX for comment component
     - In index.js, refactored return div into multi-line JSX
     - Added selector of className ui container comments (Semantic UI tag)
       to wrap comments
       - Added a comment div to wrap all elements of comments
        - Added a link going nowhere with className avatar (SUI) for
          clickable avatars
          - Added image rendering nothing yet for avatars
        - Added div for comment content
          - Added a link going nowhere with className author for clickable
            authors
          - Added a div with className metadata for metadata
            - Added a span with className date to render date metadata
              - Added 'Today at 6:00 PM' into the date div, static date
          - Added a div with className text for the comment body
            - Added 'Nice post!' into the comment div, static comment body
    This is a naive approach to writing JSX that is going to be refactored.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today At 6:00 PM</span>
+          </div>
+          <div className="text">
+            Nice Blog Post
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
