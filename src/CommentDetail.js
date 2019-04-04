a6a877169529872d18df47a935d47095326e113b
ebrault <emmet.brault@gmail.com>
2019-04-04 17:15:38 -0400

    Created CommentDetail Component
    Created CommentDetail component, a separate component that will take
    care of the rendering of comment bodies for our app.
      - Imported React and faker
      - Created functional component CommentDetail with props passed in
      - Returns multi-line JSX
        - Paste in comment body from index.js
      - Export CommentDetail

---
+++ src/CommentDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import faker from 'faker';
+
+const CommentDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()}/>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today At 6:00 PM</span>
+        </div>
+        <div className="text">
+          Nice Blog Post
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CommentDetail
