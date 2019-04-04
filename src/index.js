9ae7a1457c19b389606ef3b0d35bd02394bb469c
ebrault <emmet.brault@gmail.com>
2019-04-04 17:37:17 -0400

    Added Multiple Props To Children
    Added a secondary prop named 'timeAgo' to each instance of CommentDetail
    in index.js.
      - Set timeAgo prop in instances of commentDetail
      - Props now has two key/value pairs attached to it
      - Passed props down into date span as a variable with the time ago
        key accessor
      - Successful, saw changes rendered in DOM

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Emmet" />
-      <CommentDetail author="Audrey" />
+      <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" />
+      <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" />
+      <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" />
     </div>
   )
 }
