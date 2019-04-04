2e14c84bf1b21370d44d2beee04aec9b8572f63d
ebrault <emmet.brault@gmail.com>
2019-04-04 17:46:23 -0400

    Added Fourth Prop
    Added fourth prop to each instance of the child component, avatar in
    order to demonstrate that JS variables can be passed as props
      - Reimported faker into index.js
      - Assigned prop avatar to a JS variable referencing a function call to
        faker's image avatar method
      - Props now has four key value pairs associated with it
      - Replaced avatar src in CommentDetail with avatar key accessor called
        on props
      - Successful, no changes rendered to DOM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" content="Nice Post!"/>
-      <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" content="OK Post!"/>
-      <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" content="Poor Post!"/>
+      <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" content="Nice Post!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" content="OK Post!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" content="Poor Post!" avatar={faker.image.avatar()} />
     </div>
   )
 }
