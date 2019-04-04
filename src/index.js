11e9abb35c1d70c5911a232f70b5c1eba4aa0de1
ebrault <emmet.brault@gmail.com>
2019-04-04 17:41:31 -0400

    Added Third Prop
    Added third prop "content" to each instance of CommentDetail and
    assigned different content to each instance of CommentDetail
      - Props now has three key/value pairs, name, timeAgo, and content
      - Passed content prop down to child component:
        - Replaced value of text span with a variable of the content key
          accessor called on props
      - Successful, changes rendered to DOM

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" />
-      <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" />
-      <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" />
+      <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" content="Nice Post!"/>
+      <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" content="OK Post!"/>
+      <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" content="Poor Post!"/>
     </div>
   )
 }
