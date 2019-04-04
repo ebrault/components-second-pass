9ac4bbfd32902caab944c1509d18f4a382a675e9
ebrault <emmet.brault@gmail.com>
2019-04-04 18:01:18 -0400

    Created Third Component: ApprovalCard
    Created third component: ApprovalCard using Semantic UI
      - Import React
      - Create functional component ApprovalCard
      - Export ApprovalCard
      - Build ApprovalCard using Semantic UI
      - Import ApprovalCard into index.js
      - Render instance of ApprovalCard in container div
      - Successful, see ApprovalCard in container div

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <CommentDetail author="Sam" timeAgo="Today At 5:45 PM" content="Nice Post!" avatar={faker.image.avatar()} />
       <CommentDetail author="Emmet" timeAgo="Today At 2:00 AM" content="OK Post!" avatar={faker.image.avatar()} />
       <CommentDetail author="Audrey" timeAgo="Yesterday At 5:00 PM" content="Poor Post!" avatar={faker.image.avatar()} />
+      <ApprovalCard />
     </div>
   )
 }
